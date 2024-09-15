2a4553e948861169b00d93c9f068165dde87b52f
nataliiasolomchak21 <natalia2001727@gmail.com>
2024-09-15 22:29:59 +0100

    style: Fix styling for checkboxes, labels and textarea

--- components/ui/label.tsx
+++ components/ui/label.tsx
@@ -7,7 +7,7 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/lib/utils"
 
 const labelVariants = cva(
-  "text-sm text-dark-500 font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+  "text-sm text-dark-300 font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
 )
 
 const Label = React.forwardRef<
